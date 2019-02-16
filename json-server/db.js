fca17ea039b392633fe149ec9293b4b92ba758e1
Worapol Buraphan <xeastcrast@hotmail.com>
2019-02-16 16:06:07 +0700

    change json-db data

--- json-server/db.js
+++ json-server/db.js
@@ -1,7 +1,8 @@
 module.exports = () => {
   const db = Array.apply(null, { length: 30 }).map((val, i) => ({
     id: i,
-    username: "fake" + i
+    username: "user" + i,
+    password: '1234'
   }));
 
   return {
